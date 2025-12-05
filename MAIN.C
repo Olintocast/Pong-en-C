87eb241410aed5edef7f71581a4a52f0dd6c8e90
Edgard Castro <edgardolintocast@gmail.com>
2025-12-04 23:09:53 -0600

    Se agregó la bola y se corrigió un bug
    Se agregó la bola con su movimiento y colisiones.
    Se arregló el bug que impedía mover las paletas al llegar a los topes.

--- MAIN.C
+++ MAIN.C
@@ -1,5 +1,4 @@
-/* Solo se puede mover una paleta a la vez. Al llegar a las esquinas ya no
-pueden seguir moviendo */
+/* Solo se puede mover una paleta a la vez */
 
 #include <graphics.h>
 #include <mouse.h>
@@ -13,8 +12,17 @@ typedef struct{
 	int color;
 } Paletas;
 
+/* Variables configurables del juego */
+int color_bola = 8, radio_bola = 7;
 int color_del_fondo = 15;
 
+/* Movimiento inicial de la bola */
+int cambioX = 2, cambioY = 1;
+
+/* Coordenadas iniciales de la bola */
+int bolax = 200, bolay = 300;
+
+/* Coordenadas iniciales de las paletas */
 Paletas paleta1 = {10, 220, 20, 310, 9};
 Paletas paleta2 = {620, 220, 630, 310, 9};
 
@@ -25,8 +33,11 @@ void fondo(int color);
 /*  Prototipos de funciones del juego */
 void dibujar_paleta1(void);
 void dibujar_paleta2(void);
+void dibujar_bola(void);
+
 void borrar_paleta1(void);
 void borrar_paleta2(void);
+void borrar_bola(void);
 
 void main(){
 	/* Inicio del modo grafico */
@@ -38,22 +49,54 @@ void main(){
 	/* Dibuja las paletas */
 	dibujar_paleta1(); dibujar_paleta2();
 
+	/* Bucle del juego */
 	while(1){
+		/* Si la bola no ha tocado los limites se sigue moviendo */
+		if(bolax <= 640 && bolax >= 0){
+			borrar_bola();
+
+			/* Cambio de direccion si la bola toca los bordes superior o inferior */
+			if(bolay <= 0) cambioY = 1;
+			if(bolay >= 480) cambioY = -1;
+
+			/* Ccambio de direccion si la bola toca la paleta 1 */
+			if(bolax < (22 + radio_bola) && bolay > (paleta1.y1 - radio_bola) && bolay < (paleta1.y2 + radio_bola)){
+				cambioX = 2;
+			}
+
+            /* Cambio de direccion si la bola toca la paleta 2 */
+			if(bolax > (618 - radio_bola) && bolay > (paleta2.y1 - radio_bola) && bolay < (paleta2.y2 + radio_bola)){
+				cambioX = -2;
+			}
+
+			/* Se actualizan sus coordenadas */
+			bolax += cambioX;
+			bolay += cambioY;
+
+			dibujar_bola();
+		} else {
+			/* La bola toco los limites y se termina el juego */
+			setcolor(2);
+			settextstyle(1, 0, 5);
+			outtextxy(155, 220, "JUEGO TERMINADO");
+			delay(2500);
+			break;
+		}
+
 		if(kbhit()){
 			int c = getch();
 			int cambio; /* Para sumar o restar a las coordenadas Y */
 
 			/* Controles de la paleta 1 */
 			if(c==87 || c==119 || c==83 || c==115){ /* W o w o S o s*/
-				if(paleta1.y1 > 10){
-
+				if(c==87 || c==119) cambio = -5; else cambio = 5; /* Se establece el movimiento segun la tecla (arriba/abajo) */
+				if(paleta1.y1 + cambio >= 0 && paleta1.y2 + cambio <= 480){ /* Se mueve si hay espacio */
 					/* Se borra la paleta */
 					borrar_paleta1();
 
 					/* Se actualizan coordenadas */
-					if(c==87 || c==119) cambio = -5; else cambio = 5;
-					paleta1.y1 -= 5;
-					paleta1.y2 -= 5;
+					paleta1.y1 += cambio;
+					paleta1.y2 += cambio;
 
 					/* Se redibuja */
 					dibujar_paleta1();
@@ -62,12 +105,12 @@ void main(){
 
 			/* Controles de la paleta 2 */
 			if(c==72 || c==80){ /* Flecha arriba o flecha abajo*/
-				if(paleta2.y1 > 10 && paleta2.y2 < 470){
+				if(c==72) cambio = -5; else cambio = 5; /* Se establece el movimiento segun la tecla (arriba/abajo) */
+				if(paleta2.y1 + cambio >= 0 && paleta2.y2 + cambio <= 480){ /* Se mueve si hay espacio */
 					/* Se borra la paleta */
 					borrar_paleta2();
 
 					/* Se actualizan coordenadas */
-					if(c==72) cambio = -5; else cambio = 5;
 					paleta2.y1 += cambio;
 					paleta2.y2 += cambio;
 
@@ -108,6 +151,11 @@ void dibujar_paleta2(void){
 	bar(paleta2.x1, paleta2.y1, paleta2.x2, paleta2.y2);
 }
 
+void dibujar_bola(void){
+	setfillstyle(1, color_bola);
+	fillellipse(bolax, bolay, radio_bola, radio_bola);
+}
+
 void borrar_paleta1(void){
 	setfillstyle(1, color_del_fondo);
 	bar(paleta1.x1, paleta1.y1, paleta1.x2, paleta1.y2);
@@ -116,4 +164,10 @@ void borrar_paleta1(void){
 void borrar_paleta2(void){
 	setfillstyle(1, color_del_fondo);
 	bar(paleta2.x1, paleta2.y1, paleta2.x2, paleta2.y2);
+}
+
+void borrar_bola(void){
+	setcolor(15);
+	setfillstyle(1, 15);
+	fillellipse(bolax, bolay, radio_bola, radio_bola);
 }
